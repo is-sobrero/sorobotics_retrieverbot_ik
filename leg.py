770a6a27c00edfd31a70b7702defffbab7bb2039
Federico Runco <federico.runco@gmail.com>
2021-02-19 23:48:20 +0100

    updated leg definition

--- leg.py
+++ leg.py
@@ -4,10 +4,14 @@
 import uuid 
 
 class Leg:
-    def __init__(self, base_address, right_axis=False, scale_factor=1, shoulder_offset=0, back_axis=False):
+    def __init__(self, base_address, right_axis=False, scale_factor=1, shoulder_offset=0, back_axis=False, offsets=[0,0,0]):
         self.base_address = base_address
         self.right_axis = right_axis
         self.scale_factor = scale_factor
         self.shoulder_offset = shoulder_offset
         self.leg_id = uuid.uuid4()
         self.back_axis = back_axis
+        self.alfa_offset = offsets[0]
+        self.beta_offset = offsets[1]
+        self.gamma_offset = offsets[2]
+        self.invert_shoulder = (back_axis and not right_axis) or (not back_axis and right_axis)
