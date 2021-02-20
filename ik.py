737b2d4e9608878048c065cb7bb48441fb07cdf2
Federico Runco <federico.runco@gmail.com>
2021-02-20 12:08:08 +0100

    Added support for simulated environments in ik_solver

--- ik.py
+++ ik.py
@@ -14,14 +14,16 @@ class IKSolver:
     STD_SHIN_LEN = 14
     HIP_OFFSET = 5.5
 
-    def __init__(self, servo_handler, use_interpolator=False):
-        if not type(servo_handler) is ServoHandler:
+    def __init__(self, servo_handler, use_interpolator=False, simulated_environment=False):
+        if not type(servo_handler) is ServoHandler and not simulated_environment:
             raise Exception("Type of servo_handler must be ServoHandler")
         
-        self.servo_handler = servo_handler
+        if not simulated_environment:
+            self.servo_handler = servo_handler
         self.use_interpolator = use_interpolator
+        self.simulated_environment = simulated_environment
 
-    def solve(self, leg, x, y, z):
+    def solve(self, leg, x, y, z, output_deg=False):
         if not type(leg) is Leg:
             raise TypeError("Type of leg must be Leg")   
         # P(x,y,z) O(0,0,0)
@@ -44,21 +46,28 @@ class IKSolver:
         alpha_cos = (self.STD_THIGH_LEN**2 + self.STD_SHIN_LEN**2 - new_z**2) / (2*self.STD_THIGH_LEN*self.STD_SHIN_LEN)
         alpha = math.acos(alpha_cos)
         
-        alfa_deg = alpha * 180 / math.pi
+        alpha_deg = alpha * 180 / math.pi
         beta_deg = beta * 180 / math.pi
         gamma_deg = gamma * 180 / math.pi
 
-        final_alfa = leg.alfa_offset + (alfa_deg if not leg.right_axis else 180 - alfa_deg)
+        if self.simulated_environment:
+            if output_deg:
+                return alpha_deg, beta_deg, gamma_deg
+            else:
+                return alpha, beta, gamma
+
+        final_alfa = leg.alfa_offset + (alpha_deg if not leg.right_axis else 180 - alpha_deg)
         final_beta = leg.beta_offset + (beta_deg if not leg.right_axis else 180 - beta_deg)
         final_gamma = leg.gamma_offset + (gamma_deg if not leg.invert_shoulder else 180 - gamma_deg)
 
         if final_alfa < 0:
-            raise ServoDomainError("Alfa angle of leg is not positive", leg)   
+            raise ServoDomainError("Alpha angle of leg is not positive", leg)   
         if final_beta < 0:
             raise ServoDomainError("Beta angle of leg is not positive", leg)   
         if final_gamma < 0:
             raise ServoDomainError("Gamma angle of leg is not positive", leg)   
 
-        self.servo_handler.write_channel(leg.base_address, final_gamma, leg.scale_factor)
-        self.servo_handler.write_channel(leg.base_address + 4, final_beta, leg.scale_factor)
-        self.servo_handler.write_channel(leg.base_address + 8, final_alfa, leg.scale_factor)
+        if not self.simulated_environment:
+            self.servo_handler.write_channel(leg.base_address, final_gamma, leg.scale_factor)
+            self.servo_handler.write_channel(leg.base_address + 4, final_beta, leg.scale_factor)
+            self.servo_handler.write_channel(leg.base_address + 8, final_alfa, leg.scale_factor)
